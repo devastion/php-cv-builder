c5b09195e9ea768abc20daaa1e12c2325a4efd4e
Dimitar Banev <banev_dimitar@mail.com>
2022-01-26 04:23:49 +0200

    Fixed hrefs

--- includes/footer.php
+++ includes/footer.php
@@ -5,19 +5,19 @@
         <?php
         if (isset($_SESSION["sessionid"])) {
             echo 'Welcome '.$_SESSION["sessionuser"];
-            echo '<a href="./index.php" class="link-primary mx-3">Home</a>
-        <a href="./includes/logout.php" class="link-primary mx-3">Logout</a>
-        <a href="./history.php" class="link-primary mx-3">History</a>
+            echo '<a href="index.php" class="link-primary mx-3">Home</a>
+        <a href="includes/logout.php" class="link-primary mx-3">Logout</a>
+        <a href="history.php" class="link-primary mx-3">History</a>
         <a href="https://www.github.com/devastion" target="_blank" class="link-dark mx-3">Github</a>';
         } else {
-            echo '<a href="./index.php" class="link-primary mx-3">Home</a>
-        <a href="./login.php" class="link-primary mx-3">Login / Register</a>
+            echo '<a href="index.php" class="link-primary mx-3">Home</a>
+        <a href="login.php" class="link-primary mx-3">Login / Register</a>
         <a href="https://www.github.com/devastion" target="_blank" class="link-dark mx-3">Github</a>';
         }
         ?>
     </div>
 </footer>
 </div>
-<script src="./template/index.51cd1bbc.js"></script>
+<script src="template/index.51cd1bbc.js"></script>
 </body>
 </html>
